e6aea8ea2418400fd1357839b5fd2463a3103b53
Ernest V <ernest.viol@gmail.com>
2026-02-03 19:44:01 -0800

    Add date object to forecast element
    Showing only month and day for the object.

--- src/index.js
+++ src/index.js
@@ -21,15 +21,64 @@ await loadData();
 
 async function loadData() {
   const pos = await getLatLong();
-  const cityName = getCity(pos.latitude, pos.longitude);
-  const weatherData = getWeatherDataByLatLong(pos.latitude, pos.longitude);
+  const cityNameData = getCity(pos.latitude, pos.longitude);
+  const weatherData = await getWeatherDataByLatLong(
+    pos.latitude,
+    pos.longitude,
+  );
+  const currentCityEl = document.getElementById("current-city-value");
+  const currentTempEl = document.getElementById("current-temperature-value");
+  const weeklyWeatherEl = document.querySelector(".weekly-weather");
 
-  // const weatherData = await getWeatherData(pos);
-  // console.log(weatherData);
-  // const weatherDescription = document.getElementById("weather__description");
-  // weatherDescription.innerText = weatherData.description;
+  Promise.all([cityNameData, weatherData]).then((arr) => {
+    const cityName = arr[0];
+    const weatherData = arr[1];
+
+    currentCityEl.innerText = cityName;
+    currentTempEl.innerText = weatherData.currentConditions.temp;
+    for (const day of weatherData.days) {
+      weeklyWeatherEl.appendChild(createDayForecastElement(day));
+    }
+  });
 }
 
-function createDayForecastElement() {}
+function createDayForecastElement(day) {
+  console.log(day);
+  let dayData = {
+    high: day.tempmax,
+    low: day.tempmin,
+    icon: day.icon,
+    date: new Date(day.datetime).toLocaleDateString(undefined, {
+      month: "2-digit",
+      day: "2-digit",
+    }),
+  };
+
+  const forecastEl = document.createElement("div");
+  forecastEl.className = "day-forecast";
+
+  const tempDateEl = document.createElement("span");
+  tempDateEl.className = "forecast-date";
+  tempDateEl.innerText = dayData.date;
+
+  const tempHighEl = document.createElement("span");
+  tempHighEl.className = "forecast-high";
+  tempHighEl.innerText = dayData.high;
+
+  const weatherIconEl = document.createElement("span");
+  weatherIconEl.className = "forecast-icon";
+  weatherIconEl.innerText = dayData.icon;
+
+  const tempLowEl = document.createElement("span");
+  tempLowEl.className = "forecast-low";
+  tempLowEl.innerText = dayData.low;
+
+  forecastEl.appendChild(tempDateEl);
+  forecastEl.appendChild(tempHighEl);
+  forecastEl.appendChild(weatherIconEl);
+  forecastEl.appendChild(tempLowEl);
+
+  return forecastEl;
+}
 
 function createCurrentConditionElement() {}
